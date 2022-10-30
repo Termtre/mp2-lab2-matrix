c5b7ac5853e8718404b48a36ec3dd6f5990f54a4
Дмитрий <fromira@MacBook-Air-Dmitrij.local>
2022-10-30 15:25:41 +0300

    part of googletest

--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -51,12 +51,14 @@ TEST(TDynamicVector, can_get_size)
 
 TEST(TDynamicVector, throws_when_set_element_with_negative_index)
 {
-  ADD_FAILURE();
+    TDynamicVector<int> v(1);
+    ASSERT_ANY_THROW(v.at(-1));
 }
 
 TEST(TDynamicVector, throws_when_set_element_with_too_large_index)
 {
-  ADD_FAILURE();
+    TDynamicVector<int> v(1);
+    ASSERT_ANY_THROW(v.at(1));
 }
 
 TEST(TDynamicVector, can_assign_vector_to_itself)
@@ -81,17 +83,22 @@ TEST(TDynamicVector, can_assign_vectors_of_different_size)
 
 TEST(TDynamicVector, compare_equal_vectors_return_true)
 {
-  ADD_FAILURE();
+    TDynamicVector<int> a(2);
+    TDynamicVector<int> b(2);
+    EXPECT_EQ(true, a == b);
 }
 
 TEST(TDynamicVector, compare_vector_with_itself_return_true)
 {
-  ADD_FAILURE();
+    TDynamicVector<int> a(2);
+    EXPECT_EQ(true, a == a);
 }
 
 TEST(TDynamicVector, vectors_with_different_size_are_not_equal)
 {
-  ADD_FAILURE();
+    TDynamicVector<int> a(2);
+    TDynamicVector<int> b(1);
+    EXPECT_EQ(false, a == b);
 }
 
 TEST(TDynamicVector, can_add_scalar_to_vector)
@@ -111,12 +118,16 @@ TEST(TDynamicVector, can_multiply_scalar_by_vector)
 
 TEST(TDynamicVector, can_add_vectors_with_equal_size)
 {
-  ADD_FAILURE();
+    TDynamicVector<int> v1(1);
+    TDynamicVector<int> v2(2);
+    ASSERT_ANY_THROW(v1 + v2);
 }
 
 TEST(TDynamicVector, cant_add_vectors_with_not_equal_size)
 {
-  ADD_FAILURE();
+    TDynamicVector<int> v1(1);
+    TDynamicVector<int> v2(2);
+    ASSERT_ANY_THROW(v1 + v2);
 }
 
 TEST(TDynamicVector, can_subtract_vectors_with_equal_size)
@@ -126,16 +137,23 @@ TEST(TDynamicVector, can_subtract_vectors_with_equal_size)
 
 TEST(TDynamicVector, cant_subtract_vectors_with_not_equal_size)
 {
-  ADD_FAILURE();
+    TDynamicVector<int> v1(1);
+    TDynamicVector<int> v2(2);
+    ASSERT_ANY_THROW(v1 - v2);
 }
 
 TEST(TDynamicVector, can_multiply_vectors_with_equal_size)
 {
-  ADD_FAILURE();
+    TDynamicVector<int> v1(2);
+    TDynamicVector<int> v2(2);
+    TDynamicVector<int> v3(2);
+    EXPECT_EQ(v3, v1 * v2);
 }
 
 TEST(TDynamicVector, cant_multiply_vectors_with_not_equal_size)
 {
-  ADD_FAILURE();
+    TDynamicVector<int> v1(1);
+    TDynamicVector<int> v2(2);
+    ASSERT_ANY_THROW(v1 * v2);
 }
 
